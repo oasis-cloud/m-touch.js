b208675fe91cd622288d400d727307875680f312
wangbei6 <wangbei6@jd.com>
2019-11-15 16:06:03 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-import Config from './config';
-console.log(Config);
+let routersArray = {}
+function routersDes(path) {
+    return function routers(target: any, propertyName: string) {
+        console.log(path)
+        console.log(target)
+        console.log(propertyName)
+        routersArray[path] = {
+            target: this,
+            funcName: propertyName
+        }
+    }
+}
+
+class Employee {
+    @routersDes("/")
+    index() {
+        console.log('index')
+    }
+}
+
+const e = new Employee()
+
+routersArray['/']
